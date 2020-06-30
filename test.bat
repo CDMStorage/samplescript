145b0baf7db7575c346083daf4d23e42fe2f8c33
saravanakumar <mailofcsk@gmail.com>
2020-06-30 00:07:37 -0700

    first commit

--- test.bat
+++ test.bat
@@ -1,13 +1,23 @@
 pipeline {
-    agent { label 'master' }
+    agent any
     stages {
+ 	stage('git-checkout') {
+            steps {
+                echo "git-checkingout"
+                git 'https://github.com/CDMStorage/samplescript.git'
+            }
+        }
         stage('build') {
             steps {
                 echo "Hello World!"
+		bat 'test.bat'
                 sh "echo Hello from the shell"
                 sh "hostname"
                 sh "uptime"
             }
         }
+
+
     }
 }
+
